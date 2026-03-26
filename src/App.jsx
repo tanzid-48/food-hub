ad139da56d3de8457646e565c1ea4ea259ec4573
tanzid-48 <mdtanzid.525@gmail.com>
2026-03-26 22:58:53 +0600

    lraen useState Basic

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,29 @@
 
+import { useState } from 'react'
 import './App.css'
 
 function App() {
 
-
-return (
-  <>
-
-
-    <div>
+  const [count, setCount] = useState(0);
+  const handleInc = () =>{
+    setCount(count+1);
+  }
 
 
-
-
-
-
-
-    </div>
-
-
-
-
-
-
-  </>
-)
+  return (
+    <>
+      <div>
+      <p>Count: {count}</p>
+        <button onClick={handleInc} className='btn btn-accent'>
+        Increment
+      </button>
+      <button onClick={() => setCount(count - 1)} className='btn btn-primary'>
+        Decrement
+      </button>
+      <button onClick={() => setCount(0)}className='btn btn-accent'>Reset</button>
+      </div>
+    </>
+  )
 }
 
 export default App
